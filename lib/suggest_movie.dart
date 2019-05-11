b05d86b75cb5e297a6ffe4aaabffd85dead9d4c2
Emre Serbest <eserbest1903@gmail.com>
2019-05-11 16:00:44 +0300

    suggest-screen

--- lib/suggest_movie.dart
+++ lib/suggest_movie.dart
@@ -6,16 +6,96 @@ class SuggestScreen extends StatefulWidget {
 }
 
 class _SuggestScreenState extends State<SuggestScreen> {
+  String secilenTur;
+  String secilenRadio;
+
   @override
   Widget build(BuildContext context) {
+    double height= MediaQuery.of(context).size.height ;
     return Scaffold(
       appBar: AppBar(
         title: Text("SUGGEST A MOVIE"),
       ),
-      body: Row(
+      body: Column(
         children: <Widget>[
-          Column(
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: RadioListTile(
+                  activeColor: Colors.purple,
+                  title: Text("Rastgele"),
+                  value: "Rastgele",
+                  groupValue: secilenRadio,
+                  onChanged: (secim) {
+                    setState(() {
+                      secilenRadio = secim;
+                      print(secim);
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: RadioListTile(
+                  activeColor: Colors.purple,
+                  title: Text("Türe Göre"),
+                  value: "TüreGöre",
+                  groupValue: secilenRadio,
+                  onChanged: (secim) {
+                    setState(() {
+                      secilenRadio = secim;
+                      print(secim);
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 60),
+                child: DropdownButton<String>(
+                  items: [
+                    DropdownMenuItem(
+                      child: Text("Komedi"),
+                      value: "Komedi",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Korku"),
+                      value: "Korku",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Aksiyon"),
+                      value: "Aksiyon",
+                    ),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      secilenTur = value;
+                      print(value);
+                    });
+                  },
+                  hint: Text("Bir Tür Seçin"),
+                  value: secilenTur,
+                ),
+              ),
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(bottom: height-300) ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: RaisedButton(
+                  color: Colors.pink,
+                  child: Text("SUGGEST",style: TextStyle(color: Colors.white),),
+                  
+                  onPressed: (){
 
+                  },
+                ),
+              ),
+            ],
           )
         ],
       ),
